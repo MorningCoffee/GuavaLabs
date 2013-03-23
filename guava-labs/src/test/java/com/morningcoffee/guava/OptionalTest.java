6d091a084c14fd9bd63b39305f3015ba3dbb288c
Oleh Sklyarenko <tyne.oskl@gmail.com>
2013-03-23 12:45:34 +0200

    Added a few basic tests around Optional#of API

--- guava-labs/src/test/java/com/morningcoffee/guava/OptionalTest.java
+++ guava-labs/src/test/java/com/morningcoffee/guava/OptionalTest.java
@@ -1,7 +1,12 @@
 package com.morningcoffee.guava;
 
 
+import com.google.common.base.Optional;
+import junit.framework.Assert;
 import org.junit.Test;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
 
 /**
  * TODO Document this type
@@ -10,10 +15,37 @@ import org.junit.Test;
  * @author osklyarenko
  */
 public class OptionalTest {
+    /**
+     * Optional#of doesn't allow null passed as a value. Fails fast with NPE
+     * @throws Exception
+     */
+    @Test(expected = NullPointerException.class)
+    public void testOptionalOf1() throws Exception {
+        Optional.of(null);
+    }
+
+    /**
+     * Optional#isPresent method tests if actual value is there
+     * @throws Exception
+     */
     @Test
-    public void testName() throws Exception {
+    public void testOptionalOf2() throws Exception {
 
-        //sdsds
+        final Optional<String> optionalString = Optional.of("string reference");
 
+        assertTrue(optionalString.isPresent());
+        assertTrue(optionalString.get().equals("string reference"));
+    }
+
+    /**
+     * Optional#toString does not return value.toString
+     * @throws Exception
+     */
+    @Test
+    public void testOptionalToString() throws Exception {
+        final Optional<String> optionalString = Optional.of("string");
+        assertFalse(optionalString.toString().equals("string"));
+
+        System.out.println(optionalString.toString());
     }
 }
