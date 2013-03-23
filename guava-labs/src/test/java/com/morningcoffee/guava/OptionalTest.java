622f6ed2c2cf6abfe1147bc3bdc5cea851c274fd
Oleh Sklyarenko <tyne.oskl@gmail.com>
2013-03-23 11:28:53 +0200

    Added sample junit test

---
+++ guava-labs/src/test/java/com/morningcoffee/guava/OptionalTest.java
@@ -0,0 +1,18 @@
+package com.morningcoffee.guava;
+
+
+import org.junit.Test;
+
+/**
+ * TODO Document this type
+ * Date: 3/23/13
+ *
+ * @author osklyarenko
+ */
+public class OptionalTest {
+    @Test
+    public void testName() throws Exception {
+
+
+    }
+}
