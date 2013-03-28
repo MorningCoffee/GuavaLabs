0e81222a835c5696c6ca059801addbd5eeaf4e5b
Oleh Sklyarenko <tyne.oskl@gmail.com>
2013-03-28 13:54:49 +0200

    Added simple event bus test

--- guava-labs/src/test/java/com/morningcoffee/guava/EventBusTest.java
+++ guava-labs/src/test/java/com/morningcoffee/guava/EventBusTest.java
@@ -1,5 +1,14 @@
 package com.morningcoffee.guava;
 
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static junit.framework.Assert.fail;
+
 /**
  * Exploring EventBus type
  * Date: 3/28/13
@@ -7,4 +16,39 @@ package com.morningcoffee.guava;
  * @author osklyarenko
  */
 public class EventBusTest {
+
+    private EventBus bus = new EventBus("EventBus");
+
+    @Test
+    public void testSimpleEvent() throws Exception {
+        CountDownLatch latch = new CountDownLatch(1);
+
+        bus.register(new Listener(latch));
+        bus.post(new Event("event"));
+        if (!latch.await(100, TimeUnit.MILLISECONDS)) {
+            fail();
+        }
+    }
+
+    static class Event {
+        private final String name;
+
+        Event(String name) {
+            this.name = name;
+        }
+    }
+
+    static class Listener {
+        final CountDownLatch latch;
+
+        Listener(CountDownLatch latch) {
+            this.latch = latch;
+        }
+        @Subscribe
+        public void listen(Event e) {
+
+            latch.countDown();
+        }
+
+    }
 }
